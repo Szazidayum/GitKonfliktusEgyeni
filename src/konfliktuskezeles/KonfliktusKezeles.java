e1869cab043ee7a8c5773898d5e91743f7dc2d3c
Szazidayum <95658980+Szazidayum@users.noreply.github.com>
2022-03-07 14:47:04 +0100

    3. név deklarálása

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -1,8 +1,10 @@
 package konfliktuskezeles;
 public class KonfliktusKezeles {
     public static void main(String[] args) {
+        int i = 7;
         String nev1="MSz";
         String nev2="x.y";
+        int nev3;
     }
     
 }
